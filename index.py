aa172d3c96e5fe3e79c6877745660f05d2e30ef0
Eugene Z <eugene.zabuzov@gmail.com>
2022-03-10 17:50:17 +0500

    Homework

---
+++ index.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def make_report(log_file_name, report_template_file, report_output_file_name):
+    '''
+    :param log_file_name: Имя файла логов
+    :param report_template_file: имя файла-шаблона для отчета
+    :param report_output_file_name: имя файла отчета
+    :return: создает отчет в формате report.xlsx
+    '''
+    # Чтение и анализ данных из файла excel
+
+    # Открываем файл шаблона отчета report_template.xlsx
+
+    # Выполняем запись данных в объект wb
+
+    # Сохраняем файл отчета
+
